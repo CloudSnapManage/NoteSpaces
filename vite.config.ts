5abc3445b20bc192c83611392a2801adbd941349
Shrijan Paudel <cloudsnapmanage@gmail.com>
2025-11-29 13:12:08 +0545

    Set base path for GitHub Pages deployment
    Update base path for GitHub Pages deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // GitHub Pages deployment config
-  // Replace 'repo-name' with your actual repository name if deploying to https://user.github.io/repo-name/
-  // If deploying to https://user.github.io/, leave it as '/'
+  // 'base: "./"' ensures assets are linked relatively. 
+  // This is required for GitHub Pages (e.g. https://username.github.io/repo-name/)
   base: './', 
 });
